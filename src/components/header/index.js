0dcc8d91eefb63ec23de61703d12f0dc6fb79a51
Lucas-Vieira <lucianolucas.cntt@gmail.com>
2023-12-02 16:05:27 -0300

    adicionando projeto ao repositório

---
+++ src/components/header/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Container, Title } from "./styled";
+
+export const Header = () => {
+  return (
+    <Container>
+      <Title>LV Finanças</Title>
+    </Container>
+  );
+};
